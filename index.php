bc7a32b435556cd7b376a3f59454dd0b491a64b4
ronald <roal99@gmail.com>
2016-03-15 11:56:08 -0500

    ´primer mundo

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+<h2 id="primero" class="verde" >HOLA</h2>
+<h2 class="verde">A</h2>
+<h2 class="azul">B</h2>
+<h2 class ="azul">C</h2>
+<img class="img" src="josue.jpg" alt="Imagen bacana" width="200">
+	
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos2.css">
+</head>
+<body>
+<div class="contenedor">
+	<nav>
+		<ul class= "menu-lista">
+			<li class="menu-item">
+			<a href="" class="menu-link">INICIO</a>
+			</li>
+			<li class="menu-item">
+			<a href=""class="menu-link">BLOG</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">AVANCES</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">CONTACTENOS</a>
+			</li>
+			<li class="menu-item">
+			
+		</ul>
+	</nav>
+	<div class="slider">
+	<img src="animals_081.gif" height="350 " width="960">
+	<img src="corazonnnn.gif" alt="180" width="180">
+	<img src="corazonnnn.gif" alt="180" width="180">
+	<img src="corazonnnn.gif" alt="180" width="180">
+	<img src="corazonnnn.gif" alt="180" width="180">
+	<img src="corazonnnn.gif" alt="180" width="180">
+</div>
+</body>
+</html>
